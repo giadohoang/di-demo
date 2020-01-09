bea0590c987083a619a284deb9c2a2ce198f8c2f
Gia Do <gia.dohoang@gmail.com>
2020-01-09 11:52:21 -0500

    Added different Primary Service with different @Profile

--- src/main/java/com/springframework/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/com/springframework/didemo/services/PrimarySpanishGreetingService.java
@@ -1,16 +1,17 @@
 package com.springframework.didemo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService{
+public class PrimarySpanishGreetingService implements GreetingService{
 
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Servicio de Saludo Primario";
     }
 }
