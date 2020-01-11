1ed467d7d51c5f02108e1d231f8b78439287fba2
Gia Do <gia.dohoang@gmail.com>
2020-01-11 09:47:33 -0500

    Applying temp data source from file into Bean

--- src/main/java/com/springframework/didemo/DiDemoApplication.java
+++ src/main/java/com/springframework/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import com.springframework.didemo.controllers.ConstructorInjectedController;
 import com.springframework.didemo.controllers.MyController;
 import com.springframework.didemo.controllers.PropertyInjectedController;
 import com.springframework.didemo.controllers.GetterInjectedController;
+import com.springframework.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,11 +16,9 @@ public class DiDemoApplication {
       ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
         MyController controller = (MyController) ctx.getBean("myController");
 
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-        System.out.println( controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        System.out.println(fakeDataSource.getUser());
     }
 
 }
